d32550c1f738ee2997339b0fe98badaba2da7d28
Eugen2510 <Lugovoj20@gmail.com>
2024-09-19 20:50:16 +0200

    code refactor

--- src/main/java/ua/everybuy/routing/controler/external/ChangeUserInfoController.java
+++ src/main/java/ua/everybuy/routing/controler/external/ChangeUserInfoController.java
@@ -2,7 +2,6 @@ package ua.everybuy.routing.controler.external;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.CachePut;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
